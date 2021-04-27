2dd1b95fd5016198df9e815dc5bf506a774abcaa
Renzi0n <36129793+Renzi0n@users.noreply.github.com>
2021-04-27 23:17:22 +0300

    Добавлена функция выделения задач важными

--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -5,38 +5,44 @@ export default class TodoListItem extends Component {
 
     state = {
         done: false,
+        important: false
     }
 
     onLabelClick = () => {
         this.setState({
-            done: !this.state.done
+            done: !this.state.done,
+        })
+    }
+
+    onBtnImportantClick = () => {
+        this.setState({
+            important: !this.state.important,
         })
     }
 
     render () {
-        const { label, important = false } = this.props;
-        const { done } = this.state;
+        const { label } = this.props;
+        const { done, important } = this.state;
 
         let classNames = 'todo-list-item';
         if (done) {
             classNames += ' done';
         }
 
-        const style = {
-            color: important ? 'steelblue' : 'black',
-            fontWeight: important ? 'bold' : 'normal'
-        };
+        if (important) {
+            classNames += ' important';
+        }
     
         return (
             <span className={classNames}>
                 <span className='todo-list-item-label' 
-                    style={style} 
                     onClick={this.onLabelClick}>
                     {label}
                 </span>
     
                 <button type="button"
-                        className="btn btn-outline-success btn-sm float-right">
+                        className="btn btn-outline-success btn-sm float-right"
+                        onClick={this.onBtnImportantClick}>
                     <i className="fa fa-exclamation" />
                 </button>
     
